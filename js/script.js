5852c5da437d10512716772145e4af597cb2a851
alexandro <alexandrokessek@gmail.com>
2024-11-06 00:14:58 +0700

    Checkpoint1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 // Ini JavaScripts
+//function
+//
+
+function hitungBMI(){
+    let berat = document.getElementById('berat-badan').value
+    let tinggi = document.getElementById('tinggi-badan').value
+
+    let bmi = (berat-badan/(tinggi-badan/100)^2);
+    console.log(bmi)
+}
